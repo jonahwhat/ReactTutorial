78b846e46bb59beb7a129a3fa9ee78ad4427a3fd
Jonah <105020079+jonahwhat@users.noreply.github.com>
2023-09-17 21:44:09 -0400

    Initial Commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+
+function Message() {
+    // JSX syntax
+    
+    // anything within {} brackets can be javascript code
+    let name = "Jonah";
+    return <h1> Hello {name}</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+
+function ListGroup() {
+
+    const items = [
+        "hello",
+        "world",
+        "hi"
+    ]
+
+    
+    return (
+        <>
+            <h1>Cool List</h1>
+            <ul className="list-group">
+                {items.map(item => <li key = {item} className="list-group-item">{item}</li>)}
+
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
